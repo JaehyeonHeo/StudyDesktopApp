cd3655b3ce9b409562e991496abf336a58e21fd4
JaehyeonHeo <jhheo06@gmail.com>
2021-03-09 14:31:22 +0900

    practiceWinform

---
+++ WinformApp/PracticeWinApp/PracticeWInApp/LabelTestApp/FrmMain.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabelTestApp
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            LblAutoSize.Text = LblManualsize.Text = string.Empty; 
+        }
+
+        private void BtnPushText_Click(object sender, EventArgs e)
+        {
+            string sample1 = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum nam, consequatur provident laudantium minima exercitationem nobis, iste expedita molestias nemo qui recusandae? Beatae explicabo, veritatis ullam delectus quibusdam commodi quo?";
+            string sample2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, praesentium labore doloremque rerum distinctio dolores, dicta sequi minima itaque similique molestiae nulla dolorum sit veniam nobis ducimus ullam quisquam dolorem.";
+
+            LblAutoSize.Text = sample1;
+            LblManualsize.Text = sample2; 
+        }
+    }
+}
---
+++ WinformApp/PracticeWinApp/PracticeWInApp/PracticeWInApp/Views/FrmParents.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticeWInApp.Views  // 네임스페이스 변경할때는 designer.cs 코드의 네임스페이스도 바꿔야한다.
+{
+    public partial class FrmParents : Form
+    {
+        public FrmParents()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmParents_Load(object sender, EventArgs e)
+        {
+            this.ClientSize = new Size(800, 600);
+
+            FrmChild frm = new FrmChild();
+            this.AddOwnedForm(frm);    // 부모창 위에 자식창이 뜨도록 하는것 
+
+            frm.Show();   // Show()는 modaless 이다. , Showdialog()는 madal
+
+        }
+    }
+}
