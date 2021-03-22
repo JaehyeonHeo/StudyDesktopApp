13adc6573cd9eca4779c64cfe5659829622974b2
JaehyeonHeo <jhheo06@gmail.com>
2021-03-22 17:24:41 +0900

    WpfPracticeApp 커밋 3/22

---
+++ WPF_App/WpfAdvBank/WpfPracticeApp/BusinessLogic/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfPracticeApp.BusinessLogic
+{
+    public class Car : Notifier
+    {
+       // public double Speed { get; set;  }  아래 두개와 완전 같은것 
+        private double speed; 
+        public double Speed 
+        { 
+            get { return speed;  }
+            set 
+            {
+                if (value > 350)
+                {
+                    speed = 350;
+                }
+                else
+                {
+                    speed = value;
+                }
+                
+                OnPropertyChanged("Speed"); // 속성값 변경된 것을 알려줌 (프로그램에)
+            }  
+        }
+        private Color mainColor
+        {
+            get { return MainColor;  }
+            set
+            {
+                mainColor = value;
+                OnPropertyChanged("Title"); 
+            }
+        }
+
+        public Color MainColor { get; set; }
+
+        public Human Driver { get; set;  }
+    }
+
+    public class Human
+    {
+        public string Name { get; set;  }
+
+        public bool HasDriveLicense { get; set;  }
+    }
+}
